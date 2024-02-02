34cf461b09ae8489ff42b36376169902a2ac4e33
mortenaho <mortenaho@gmail.com>
2024-02-02 19:25:31 +0330

    add product controller and refactor login controller

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import APIRouter, FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from Controllers.LoginController import loginRoutes
 from Controllers.ProductController import productRoutes
 
 app = FastAPI()
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(loginRoutes)
 app.include_router(productRoutes)
